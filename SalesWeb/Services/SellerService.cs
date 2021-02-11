56648ed92ba5d1272e6596192eb606bd14616589
Lucas Morato <luucasmorato@gmail.com>
2021-02-10 21:53:56 -0300

    edit seller

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWeb.Data;
 using SalesWeb.Models;
+using SalesWeb.Services.Exceptions;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -33,9 +34,46 @@ namespace SalesWeb.Services
 
         public void Remove(int id)
         {
-                var obj =  _context.Seller.Find(id);
-                _context.Seller.Remove(obj);
-                _context.SaveChangesAsync();
+            var obj =  _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChangesAsync();
         }
+
+        public void Remove(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
+        public void Update(Seller obj)
+        {
+            bool hasAny = _context.Seller.Any(x => x.Id == obj.Id);
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
