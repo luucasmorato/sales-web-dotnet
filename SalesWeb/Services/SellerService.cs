02e221780e9509649e2462bb4fadb165cd37162f
Lucas Morato <luucasmorato@gmail.com>
2021-02-10 22:35:32 -0300

    add async methods

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -4,6 +4,7 @@ using SalesWeb.Models;
 using SalesWeb.Services.Exceptions;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesWeb.Services
 {
@@ -16,50 +17,39 @@ namespace SalesWeb.Services
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public async Task<List<Seller>> FindAllAsync()
         {
-            return _context.Seller.ToList();
+            return await _context.Seller.ToListAsync();
         }
 
-        public void Insert(Seller obj)
+        public async Task InsertAsync(Seller obj)
         {
             _context.Add(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public Seller FindById(int id)
+        public async Task<Seller> FindByIdAsync(int id)
         {
-            return  _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
         }
 
-        public void Remove(int id)
+        public async Task RemoveAsync(int id)
         {
-            var obj =  _context.Seller.Find(id);
-            _context.Seller.Remove(obj);
-            _context.SaveChangesAsync();
-        }
-
-        public void Remove(Seller obj)
-        {
-            if(!_context.Seller.Any(x => x.Id == obj.Id))
-            {
-                throw new NotFoundException("Id not found");
-            }
-
             try
             {
-                _context.Update(obj);
-                _context.SaveChanges();
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
             }
-            catch(DbUpdateConcurrencyException e)
+            catch (DbUpdateException e)
             {
-                throw new DbConcurrencyException(e.Message);
+                throw new IntegrityException("Can't delete seller because he/she has sales");
             }
         }
 
-        public void Update(Seller obj)
+        public async Task UpdateAsync(Seller obj)
         {
-            bool hasAny = _context.Seller.Any(x => x.Id == obj.Id);
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
             if (!hasAny)
             {
                 throw new NotFoundException("Id not found");
@@ -67,13 +57,12 @@ namespace SalesWeb.Services
             try
             {
                 _context.Update(obj);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
             }
         }
-
     }
 }
